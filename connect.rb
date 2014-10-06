34997bbbbdb6341a621cb4a7ec822493478bf1b0
Dave Guarino <dave@codeforamerica.org>
2014-10-06 16:10:16 -0700

    When user calls, play an s3 voice file (green)

--- connect.rb
+++ connect.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
+require 'twilio-ruby'
 
 class Connect < Sinatra::Base
   post '/' do
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play 'https://s3-us-west-1.amazonaws.com/cfa-health-connect/initial_call_response.wav'
+    end
+    response.text
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 describe Connect do
   describe 'initial call' do
-    it 'responds' do
+    before do
       post '/'
-      expect(last_response.status). to eq(200)
+    end
+
+    it 'responds' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'plays a voice file to the user' do
+      parsed_response = Nokogiri.parse(last_response.body)
+      url = parsed_response.xpath('//Response//Play').children[0].text
+      expect(url).to eq('https://s3-us-west-1.amazonaws.com/cfa-health-connect/initial_call_response.wav')
     end
   end
 end
